2c92dbb789d611142592b633df8a04f2686f99b0
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-09-01 00:29:12 +0200

    Time: 32 ms (38.25%), Space: 24 MB (83.52%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -3,14 +3,14 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         std::vector<std::vector<std::string>> global_wektor;
         std::unordered_map<std::string, std::vector<string>> global_mapa;
-        std::unordered_map<char, int> mapa;
-        std::string ptr;
+        std::string* ptr = new std::string;
 
         for(const auto& word : strs){
-            ptr = word;
-            sort(ptr.begin(), ptr.end());
-            global_mapa[ptr].push_back(word);
+            *ptr = word;
+            sort(ptr->begin(), ptr->end());
+            global_mapa[*ptr].push_back(word);
         }
+        delete ptr;
         for(const auto& [word, wektor] : global_mapa){
             global_wektor.push_back(wektor);
         }
