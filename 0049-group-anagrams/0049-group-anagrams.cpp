50b47c448fefb8a06a9cc2c92e89c6b4844525b8
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-09-01 00:29:00 +0200

    Time: 24 ms (79.46%), Space: 24.1 MB (75.57%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -4,11 +4,12 @@ public:
         std::vector<std::vector<std::string>> global_wektor;
         std::unordered_map<std::string, std::vector<string>> global_mapa;
         std::unordered_map<char, int> mapa;
+        std::string ptr;
 
         for(const auto& word : strs){
-            std::unique_ptr<string> ptr = std::make_unique<string>(word);
-            sort(ptr->begin(), ptr->end());
-            global_mapa[*ptr].push_back(word);
+            ptr = word;
+            sort(ptr.begin(), ptr.end());
+            global_mapa[ptr].push_back(word);
         }
         for(const auto& [word, wektor] : global_mapa){
             global_wektor.push_back(wektor);
