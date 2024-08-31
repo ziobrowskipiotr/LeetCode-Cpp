84ad71f8b69c81ee0c2a5c5a8fdadb3639cd13f9
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-08-31 15:26:41 +0200

    Time: 8 ms (34.43%), Space: 9.6 MB (32.55%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        std::unordered_map<char, char> mapa;
+        std::unordered_map<char, char> reverse_mapa;
+        for(int i=0; i<s.length(); i++){
+            if(mapa.count(s[i]) == 0 && reverse_mapa.count(t[i]) == 0){
+                mapa[s[i]] = t[i];
+                reverse_mapa[t[i]] = s[i];
+            }
+            else if(mapa.count(s[i]) == 0 && reverse_mapa.count(t[i]) != 0){
+                return false;
+            }
+            else if(mapa[s[i]] == t[i]){
+                continue;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+};
