bab818b55595443013d3b36ed67f5f1d0b360af6
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-09-02 01:16:15 +0200

    Time: 3 ms (41.29%), Space: 8.3 MB (19.87%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
     bool isHappy(int n) {
-        int result;
-        int data;
+        int* result = new int(0);
+        int* data = new int(n);
         std::unordered_map<int, int> hashmap;
         while(n != 1){
             if(hashmap.count(n) == 0){
-                result = 0;
-                data = n;
-                while(data != 0){
-                    result += (data%10) * (data%10);
-                    data /= 10;
+                while(*data != 0){
+                    *result += (*data%10) * (*data%10);
+                    *data /= 10;
                 }
-                hashmap[n] = result;
-                n = result;
+                hashmap[n] = *result;
+                n = *result;
+                *result = 0;
+                *data = n;
             }
             else{
+                delete result;
+                delete data;
                 return false;
             }
         }
+        delete result;
+        delete data;
         return true;
     }
 };
