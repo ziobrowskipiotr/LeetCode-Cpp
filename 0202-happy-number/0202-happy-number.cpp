0866b52894895171e2dc23d194a2a8068e6193c3
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-09-02 00:20:57 +0200

    Time: 6 ms (21.05%), Space: 9.6 MB (5.2%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        int value = n;
+        int result;
+        const int zero = (int)'0';
+        std::string s_value;
+        std::unordered_map<int, int> hashmap;
+        while(value != 1){
+            if(hashmap.count(value) == 0){
+                s_value = std::to_string(value);
+                hashmap[value] = [](std::string& num, int& res, int& val, const int& zer) -> int{
+                    res = 0;
+                    for(int i=0; i<num.size(); i++){
+                        res += ((int)num[i] - zer) * ((int)num[i] - zer);
+                    }
+                    return val;
+                }(s_value, result, value, zero);
+                value = result;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+};
