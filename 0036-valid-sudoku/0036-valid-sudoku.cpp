1b708906f003d418a045d1b26b5560a9280aea58
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-09-03 20:11:40 +0200

    Time: 16 ms (64.65%), Space: 26.6 MB (5.3%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -8,20 +8,12 @@ public:
         for(int i=0; i<9; i++){
             for(int j=0; j<9; j++){
                 if(board.at(i).at(j) != '.'){
-                    if(rows[i].count(board.at(i).at(j)) == 0){
+                    if(rows[i].count(board.at(i).at(j)) == 0 &&
+                    columns[j].count(board.at(i).at(j)) == 0 && 
+                    squares[(i/3)*3 + j/3].count(board.at(i).at(j)) == 0){
                         rows[i].emplace(board.at(i).at(j));
-                        if(columns[j].count(board.at(i).at(j)) == 0){
-                            columns[j].emplace(board.at(i).at(j));
-                            if(squares[(i/3)*3 + j/3].count(board.at(i).at(j)) == 0){
-                                squares[(i/3)*3 + j/3].emplace(board.at(i).at(j));
-                            }
-                            else{
-                                return false;
-                            }
-                        }
-                        else{
-                            return false;
-                        }
+                        columns[j].emplace(board.at(i).at(j));
+                        squares[(i/3)*3 + j/3].emplace(board.at(i).at(j));
                     }
                     else{
                         return false;
