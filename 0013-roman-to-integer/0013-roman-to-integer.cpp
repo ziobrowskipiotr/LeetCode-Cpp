6ba54416a53e79038c7c72e52ab75f4dee93772e
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-09-03 23:59:29 +0200

    Time: 11 ms (55%), Space: 13 MB (61.02%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int result = 0;
+        std::unordered_map<char, int> mapa = {
+            {'M', 1000},
+            {'D', 500},
+            {'C', 100},
+            {'L', 50},
+            {'X', 10},
+            {'V', 5},
+            {'I', 1}
+        };
+        for(int i=s.size()-1; i>0; i--){
+            if(s[i-1] == 'I' && (s[i] == 'V' || s[i] == 'X')){
+                result -= 2;
+            }
+            else if(s[i-1] == 'X' && (s[i] == 'L' || s[i] == 'C')){
+                result -= 20;
+            }
+            else if(s[i-1] == 'C' && (s[i] == 'D' || s[i] == 'M')){
+                result -= 200;
+            }
+            result += mapa[s[i]];
+        }
+        result += mapa[s[0]];
+        return result;
+    }
+};
