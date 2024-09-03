9fdc1f3bd180bd7ccd4f0e3ee1c6610188711997
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-09-03 21:26:44 +0200

    Time: 14 ms (77.43%), Space: 22.4 MB (77.43%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,29 +1,27 @@
-//
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        std::unordered_map<int, std::unordered_set<char>> rows;
-        std::unordered_map<int, std::unordered_set<char>> columns;
-        std::unordered_map<int, std::unordered_set<char>> squares;
-        char num;
+        bool rows[9][9] = {false};
+        bool columns[9][9] = {false};
+        bool squares[9][9] = {false};
 
-        for(int i=0; i<9; i++){
-            for(int j=0; j<9; j++){
-                num = board.at(i).at(j);
-                if(num != '.'){
-                    if(rows[i].count(num) == 0 &&
-                    columns[j].count(num) == 0 && 
-                    squares[(i/3)*3 + j/3].count(num) == 0){
-                        rows[i].emplace(num);
-                        columns[j].emplace(num);
-                        squares[3*(i/3) + j/3].emplace(num);
-                    }
-                    else{
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.') {
+                    int num = board[i][j] - '1';
+                    int squareIndex = (i / 3) * 3 + (j / 3);
+
+                    if (rows[i][num] || columns[j][num] || squares[squareIndex][num]) {
                         return false;
                     }
+
+                    rows[i][num] = true;
+                    columns[j][num] = true;
+                    squares[squareIndex][num] = true;
                 }
             }
         }
+
         return true;
     }
 };
