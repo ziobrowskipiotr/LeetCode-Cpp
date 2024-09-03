703c10f42504a1131584b0ae0ce4079cd86d7744
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-09-03 22:53:15 +0200

    Time: 31 ms (5.75%), Space: 10.3 MB (51.91%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        int i_tab[13] = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        std::string s_tab[13] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int* p1 = i_tab;
+        std::string* p2 = s_tab;
+        int temp = 0;
+        std::string result;
+        for(int i=0; i<13; i++){
+            temp = *p1;
+            *p1 = num / *p1;
+            cout<<*p1<<endl;
+            for(int j=0; j<*p1; j++){
+                result += *p2;
+            }
+            num -= *p1 * temp;
+            p1++;
+            p2++;
+        }
+        return result;
+    }
+};
