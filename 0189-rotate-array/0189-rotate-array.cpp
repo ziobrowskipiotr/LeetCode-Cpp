9a938aefc518dab057968bfd30fc881212a36528
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-09-04 02:24:30 +0200

    Time: 20 ms (60.85%), Space: 28.7 MB (5.7%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        k %= nums.size();
+        std::vector<int> temp(nums.end()-k, nums.end());
+        nums.insert(nums.begin(), temp.begin(), temp.end());
+        for(int i=0; i<k; i++){
+            nums.pop_back();
+        }
+    }
+};
