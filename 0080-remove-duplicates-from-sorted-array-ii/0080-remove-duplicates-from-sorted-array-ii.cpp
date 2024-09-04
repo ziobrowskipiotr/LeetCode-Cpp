ead3a6cb44030a18b7769abce46c441f774b768a
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-09-04 04:53:24 +0200

    Time: 8 ms (20.89%), Space: 13.7 MB (67.54%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -1,28 +1,16 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        if(nums.size()<3){
+        if(nums.size() <= 2){
             return nums.size();
         }
-        std::vector<int>::iterator it1 = nums.begin();
-        std::vector<int>::iterator it2 = it1+1;
-        std::vector<int>::iterator it3 = it2+1;
-        while(it3 != nums.end()){
-            if(*it1 == *it2 && *it1 == *it3){
-                cout<<"it1: "<<*it1<<endl;
-                cout<<"it2: "<<*it2<<endl;
-                cout<<"it3: "<<*it3<<endl;
-                nums.erase(it3);
-            }
-            else{
-                cout<<"it1: "<<*it1<<endl;
-                cout<<"it2: "<<*it2<<endl;
-                cout<<"it3: "<<*it3<<endl;
-                it1++;
-                it2++;
-                it3++;
+        int k=2;
+        for(int i=2; i<nums.size(); i++){
+            if(nums[k-2] != nums[i]){
+                nums[k] = nums[i];
+                k++;
             }
         }
-        return nums.size();
+        return k;
     }
 };
