ecfbc33db2b6d05f8824af62fe370ec518fea44c
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-09-05 02:14:45 +0200

    Time: 24 ms (5.82%), Space: 12 MB (30.14%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -2,16 +2,15 @@ class Solution {
 public:
     string reverseWords(string s) {
         std::istringstream iss(s);
-        std::ostringstream oss;
         std::vector<std::string> wektor;
         std::string slowo;
         while(iss >> slowo){
             wektor.emplace(wektor.begin(), slowo);
         }
-        for(auto& word : wektor){
-            oss << word << " ";
+        slowo = "";
+        for(int i=0; i<wektor.size(); i++){
+            slowo += wektor[i]+" ";
         }
-        slowo = oss.str();
         return slowo.substr(0, slowo.size()-1);
     }
 };
