5af7e692cd5b7f3fd50e4d49b3abba024f48a3d5
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-09-08 21:31:40 +0200

    Time: 12 ms (55.84%), Space: 13 MB (74.27%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -4,24 +4,25 @@ public:
         if(numRows == 1){
             return s;
         }
-        std::vector<std::string> wektor(numRows, "");
+        std::string* tablica = new std::string[numRows];
         std::string result;
-        wektor[0]+=s[0];
+        
+        tablica[0]+=s[0];
         int j = 0;
-
         for(int i=1; i<s.size(); i++){
             if(((i-1)/(numRows-1))%2 == 0){
                 j++;
-                wektor.at(j) += s[i];
+                tablica[j] += s[i];
             }
             else{
                 j--;
-                wektor.at(j) += s[i];
+                tablica[j] += s[i];
             }
         }
-        for(int i=0; i<wektor.size(); i++){
-            result+=wektor[i];
+        for(int i=0; i<numRows; i++){
+            result+=tablica[i];
         }
+        delete[] tablica;
         return result;
     }
 };
