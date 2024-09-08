caea8ca8df263eda58685711ec1eb90e725124e8
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-09-08 22:56:10 +0200

    Time: 3 ms (41.86%), Space: 7.7 MB (87.38%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        return haystack.find(needle);
+        int hay_size = haystack.size();
+        int ned_size = needle.size();
+        int pointer1;
+        int pointer2;
+        for(int i=0; i<hay_size; i++){
+            if(haystack[i] == needle[0]){
+                pointer1=0;
+                pointer2=i;
+            }
+            while(pointer2<hay_size && (haystack[pointer2] == needle[pointer1])){
+                if(pointer1 == ned_size-1){
+                    return i;
+                }
+                pointer1++;
+                pointer2++;
+            }
+        }
+        return -1;
     }
 };
