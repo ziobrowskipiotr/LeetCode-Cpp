6144ae230a1ba2e5381dd7f87ff50086fab37139
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-09-11 23:27:11 +0200

    Time: 35 ms (7.42%), Space: 51.1 MB (14.37%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -12,23 +12,28 @@
 class Solution {
 public:
     TreeNode* root = new TreeNode();
-    void build(TreeNode* root, vector<int>& preorder, vector<int> inorder) {
-        auto i = find(inorder.begin(), inorder.end(), preorder.front());
+    void build(TreeNode* root, vector<int>& preorder, vector<int>& inorder) {
+        auto i = find(inorder.begin(), inorder.end(), preorder.at(0));
         if(inorder.size()>1 && preorder.size()>1 && find(inorder.begin(), inorder.end(), preorder.at(1)) != inorder.end() && i-inorder.begin() > find(inorder.begin(), inorder.end(), preorder.at(1))-inorder.begin()){
             preorder.erase(preorder.begin());
-            root->left = new TreeNode(preorder.front());
+            cout<<"z lewej: "<<preorder.at(0)<<endl;
+            root->left = new TreeNode(preorder.at(0));
             vector<int> lsub_inorder(inorder.begin(), i);
             build(root->left, preorder, lsub_inorder);
         }
         if(inorder.size()>1 && preorder.size()>1 && find(inorder.begin(), inorder.end(), preorder.at(1)) != inorder.end() && i-inorder.begin() < find(inorder.begin(), inorder.end(), preorder.at(1))-inorder.begin()){
+            cout<<"z prawej: "<<preorder.at(1)<<"bo: "<<i-inorder.begin()<<" a: "<<find(inorder.begin(), inorder.end(), preorder.at(1))-inorder.begin()<<endl;
             preorder.erase(preorder.begin());
-            root->right = new TreeNode(preorder.front());
+            root->right = new TreeNode(preorder.at(0));
             vector<int> rsub_inorder(i+1, inorder.end());   
             build(root->right, preorder, rsub_inorder);
         }
+        else{
+            cout<<"pominięto"<<endl;
+        }
     }
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        root->val = preorder.front();
+        root->val = preorder.at(0);
         build(root, preorder, inorder);
         return root;
     }
