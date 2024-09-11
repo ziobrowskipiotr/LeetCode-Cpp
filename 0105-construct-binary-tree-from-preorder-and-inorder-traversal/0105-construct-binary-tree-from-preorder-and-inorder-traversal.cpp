677743964bd63495661e9b91c7a0d1c1ac2f7f9a
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-09-11 18:17:15 +0200

    Time: 36 ms (7.12%), Space: 74 MB (10.05%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -12,23 +12,23 @@
 class Solution {
 public:
     TreeNode* root = new TreeNode();
-    void build(TreeNode* root, vector<int>& preorder, vector<int>& inorder) {
-        auto i = find(inorder.begin(), inorder.end(), preorder.at(0));
+    void build(TreeNode* root, vector<int>& preorder, vector<int> inorder) {
+        auto i = find(inorder.begin(), inorder.end(), preorder.front());
         if(inorder.size()>1 && preorder.size()>1 && find(inorder.begin(), inorder.end(), preorder.at(1)) != inorder.end() && i-inorder.begin() > find(inorder.begin(), inorder.end(), preorder.at(1))-inorder.begin()){
             preorder.erase(preorder.begin());
-            root->left = new TreeNode(preorder.at(0));
+            root->left = new TreeNode(preorder.front());
             vector<int> lsub_inorder(inorder.begin(), i);
             build(root->left, preorder, lsub_inorder);
         }
         if(inorder.size()>1 && preorder.size()>1 && find(inorder.begin(), inorder.end(), preorder.at(1)) != inorder.end() && i-inorder.begin() < find(inorder.begin(), inorder.end(), preorder.at(1))-inorder.begin()){
             preorder.erase(preorder.begin());
-            root->right = new TreeNode(preorder.at(0));
+            root->right = new TreeNode(preorder.front());
             vector<int> rsub_inorder(i+1, inorder.end());   
             build(root->right, preorder, rsub_inorder);
         }
     }
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        root->val = preorder.at(0);
+        root->val = preorder.front();
         build(root, preorder, inorder);
         return root;
     }
