07083fa989983ca67b636ba00a74ca49c36ad263
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-09-12 01:46:38 +0200

    Time: 22 ms (17.71%), Space: 51.3 MB (14.23%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -14,17 +14,23 @@ public:
     TreeNode* root = new TreeNode();
     void build(TreeNode* root, vector<int>& preorder, vector<int>& inorder) {
         auto i = find(inorder.begin(), inorder.end(), preorder.at(0));
-        if(inorder.size()>1 && preorder.size()>1 && find(inorder.begin(), inorder.end(), preorder.at(1)) != inorder.end() && i-inorder.begin() > find(inorder.begin(), inorder.end(), preorder.at(1))-inorder.begin()){
-            preorder.erase(preorder.begin());
-            root->left = new TreeNode(preorder.at(0));
-            vector<int> lsub_inorder(inorder.begin(), i);
-            build(root->left, preorder, lsub_inorder);
+        if(inorder.size()>1 && preorder.size()>1){
+            auto find1 = find(inorder.begin(), inorder.end(), preorder.at(1));
+            if(find1 != inorder.end() && i-inorder.begin() > find1-inorder.begin()){
+                preorder.erase(preorder.begin());
+                root->left = new TreeNode(preorder.at(0));
+                vector<int> lsub_inorder(inorder.begin(), i);
+                build(root->left, preorder, lsub_inorder);
+            }
         }
-        if(inorder.size()>1 && preorder.size()>1 && find(inorder.begin(), inorder.end(), preorder.at(1)) != inorder.end() && i-inorder.begin() < find(inorder.begin(), inorder.end(), preorder.at(1))-inorder.begin()){
-            preorder.erase(preorder.begin());
-            root->right = new TreeNode(preorder.at(0));
-            vector<int> rsub_inorder(i+1, inorder.end());   
-            build(root->right, preorder, rsub_inorder);
+        if(inorder.size()>1 && preorder.size()>1){
+            auto find1 = find(inorder.begin(), inorder.end(), preorder.at(1));
+            if(find1 != inorder.end() && i-inorder.begin() < find1-inorder.begin()){
+                preorder.erase(preorder.begin());
+                root->right = new TreeNode(preorder.at(0));
+                vector<int> rsub_inorder(i+1, inorder.end());   
+                build(root->right, preorder, rsub_inorder);
+            }
         }
     }
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
