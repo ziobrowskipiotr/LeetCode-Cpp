046b2799b30602aab710619c6d25e0422c0b6e55
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-09-12 02:14:33 +0200

    Time: 21 ms (20.06%), Space: 26.6 MB (98.64%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,28 +11,31 @@
  */
 class Solution {
 public:
-    void build(TreeNode* root, vector<int>& preorder, vector<int>::iterator begin, vector<int>::iterator end) {
+    void build(TreeNode* root, vector<int>& preorder, vector<int>::iterator begin, vector<int>::iterator end, int& num){
         auto i = find(begin, end, preorder.front());
-        if(end-begin>1 && preorder.size()>1){
+        if(end-begin>1 && num>1){
             auto find1 = find(begin, end, preorder.at(1));
             if(find1 != end && i > find1){
                 preorder.erase(preorder.begin());
+                num--;
                 root->left = new TreeNode(preorder.front());
-                build(root->left, preorder, begin, i);
+                build(root->left, preorder, begin, i, num);
             }
         }
         if(end-begin>1 && preorder.size()>1){
             auto find1 = find(begin, end, preorder.at(1));
             if(find1 != end && i < find1){
                 preorder.erase(preorder.begin());
+                num--;
                 root->right = new TreeNode(preorder.front());
-                build(root->right, preorder, i+1, end);
+                build(root->right, preorder, i+1, end, num);
             }
         }
     }
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         TreeNode* root = new TreeNode(preorder.front());
-        build(root, preorder, inorder.begin(), inorder.end());
+        int num = preorder.size();
+        build(root, preorder, inorder.begin(), inorder.end(), num);
         return root;
     }
 };
