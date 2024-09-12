b42685ed71d329444c87a7e7d0bf35fe1303b7c3
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-09-12 02:00:05 +0200

    Time: 15 ms (45.7%), Space: 51.4 MB (13.94%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -13,12 +13,12 @@ class Solution {
 public:
     TreeNode* root = new TreeNode();
     void build(TreeNode* root, vector<int>& preorder, vector<int>& inorder) {
-        auto i = find(inorder.begin(), inorder.end(), preorder.at(0));
+        auto i = find(inorder.begin(), inorder.end(), preorder.front());
         if(inorder.size()>1 && preorder.size()>1){
             auto find1 = find(inorder.begin(), inorder.end(), preorder.at(1));
             if(find1 != inorder.end() && i-inorder.begin() > find1-inorder.begin()){
                 preorder.erase(preorder.begin());
-                root->left = new TreeNode(preorder.at(0));
+                root->left = new TreeNode(preorder.front());
                 vector<int> lsub_inorder(inorder.begin(), i);
                 build(root->left, preorder, lsub_inorder);
             }
@@ -27,14 +27,14 @@ public:
             auto find1 = find(inorder.begin(), inorder.end(), preorder.at(1));
             if(find1 != inorder.end() && i-inorder.begin() < find1-inorder.begin()){
                 preorder.erase(preorder.begin());
-                root->right = new TreeNode(preorder.at(0));
-                vector<int> rsub_inorder(i+1, inorder.end());   
+                root->right = new TreeNode(preorder.front());
+                vector<int> rsub_inorder(i+1, inorder.end());
                 build(root->right, preorder, rsub_inorder);
             }
         }
     }
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        root->val = preorder.at(0);
+        root->val = preorder.front();
         build(root, preorder, inorder);
         return root;
     }
