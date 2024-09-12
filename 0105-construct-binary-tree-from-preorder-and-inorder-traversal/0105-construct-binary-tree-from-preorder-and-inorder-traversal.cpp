2e49278cc19d84f904e7af51425eeffefd6e310e
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-09-13 00:56:06 +0200

    Time: 4 ms (96.62%), Space: 27.4 MB (63.23%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -12,14 +12,14 @@
 class Solution {
 public:
     void complete_tree(TreeNode* &root, unordered_map<int, int*> &in_map, int* &pointer, int* &end_pointer, int* start, int* end){
-        if(pointer != end_pointer && (in_map[*(pointer+1)]>=start && in_map[*(pointer+1)]<=end)){
+        if(pointer < end_pointer && (in_map[*(pointer+1)]>=start && in_map[*(pointer+1)]<=end)){
             if(in_map[*(pointer+1)] < in_map[*pointer]){
                 pointer++;
                 root->left = new TreeNode(*(pointer));
                 complete_tree(root->left, in_map, pointer, end_pointer, start, in_map[*(pointer-1)]-1);
             }
         }
-        if(pointer != end_pointer && (in_map[*(pointer+1)]>=start && in_map[*(pointer+1)]<=end)){
+        if(pointer < end_pointer && (in_map[*(pointer+1)]>=start && in_map[*(pointer+1)]<=end)){
             if(in_map[*(pointer+1)] > in_map[*pointer]){
                 pointer++;
                 root->right = new TreeNode(*(pointer));
