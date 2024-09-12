33323c2794637cc0948da68c5f30309203e6d40f
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-09-12 03:23:36 +0200

    Time: 16 ms (40.58%), Space: 26.3 MB (99.98%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,28 +11,32 @@
  */
 class Solution {
 public:
-    void build(TreeNode* root, vector<int>& preorder, vector<int>::iterator begin, vector<int>::iterator end) {
-        auto i = find(begin, end, preorder.front());
-        if(end-begin>1 && preorder.size()>1){
-            auto find1 = find(begin, end, preorder.at(1));
+    void build(TreeNode* root, int*& pointer1, int*& pointer2, vector<int>::iterator begin, vector<int>::iterator end){
+        bool f1 = end-begin>1;
+        bool h2 = pointer1 != pointer2;
+        auto i = find(begin, end, *pointer1);
+        if(end-begin>1 && pointer1 != pointer2){
+            auto find1 = find(begin, end, *(pointer1 + 1));
             if(find1 != end && i > find1){
-                preorder.erase(preorder.begin());
-                root->left = new TreeNode(preorder.front());
-                build(root->left, preorder, begin, i);
+                root->left = new TreeNode(*(++pointer1));
+                build(root->left, pointer1, pointer2, begin, i);
             }
         }
-        if(end-begin>1 && preorder.size()>1){
-            auto find1 = find(begin, end, preorder.at(1));
+        bool f = end-begin>1;
+        bool h = pointer1 != pointer2;
+        if(end-begin>1 && pointer1 != pointer2){
+            auto find1 = find(begin, end, *(pointer1 + 1));
             if(find1 != end && i < find1){
-                preorder.erase(preorder.begin());
-                root->right = new TreeNode(preorder.front());
-                build(root->right, preorder, i+1, end);
+                root->right = new TreeNode(*(++pointer1));
+                build(root->right, pointer1, pointer2, i+1, end);
             }
         }
     }
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        TreeNode* root = new TreeNode(preorder.front());
-        build(root, preorder, inorder.begin(), inorder.end());
+        int* pointer1 = &preorder.front();
+        int* pointer2 = &preorder.back();
+        TreeNode* root = new TreeNode(*pointer1);
+        build(root, pointer1, pointer2, inorder.begin(), inorder.end());
         return root;
     }
 };
