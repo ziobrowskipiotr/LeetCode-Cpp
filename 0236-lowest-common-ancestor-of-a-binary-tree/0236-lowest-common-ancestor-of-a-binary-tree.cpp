a45e26df4024bbfc6792a224e9d1c151ee530fae
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-09-14 02:32:01 +0200

    Time: 17 ms (23.42%), Space: 16.3 MB (53.08%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,45 +9,33 @@
  */
 class Solution {
 public:
-
-    void dfs(vector<TreeNode*> &wektor, TreeNode* &p, bool &bol){
-        if(bol && wektor.back()->left == p){
-            wektor.push_back(wektor.back()->left);
-            bol = false;
-        }
-        if(bol && wektor.back()->left != nullptr){
-            wektor.push_back(wektor.back()->left);
-            dfs(wektor, p, bol);
-        }
-        if(bol && wektor.back()->right == p){
-            wektor.push_back(wektor.back()->right);
-            bol = false;
-        }
-        if(bol && wektor.back()->right != nullptr){
-            wektor.push_back(wektor.back()->right);
-            dfs(wektor, p, bol);
-        }
-        if(bol){
-            wektor.pop_back();
-        }
-    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode*> wektor_p;
-        vector<TreeNode*> wektor_q;
-        bool bol_p = true;
-        bool bol_q = true;
-        wektor_p.push_back(root);
-        wektor_q.push_back(root);
-        dfs(wektor_p, p, bol_p);
-        dfs(wektor_q, q, bol_q);
-
-        for(int i=wektor_p.size()-1; i>=0; i--){
-            for(int j=wektor_q.size()-1; j>=0; j--){
-                if(wektor_p[i] == wektor_q[j]){
-                    return wektor_p[i];
-                }
-            }
+        if(root == p || root == q){
+            return root;
         }
-        return root;
+        TreeNode* temp1;
+        TreeNode* temp2;
+        if(root->left != nullptr){
+            temp1 = lowestCommonAncestor(root->left, p, q);
+        }
+        if(root->left == nullptr){
+            temp1 = nullptr;
+        }
+        if(root->right != nullptr){
+            temp2 = lowestCommonAncestor(root->right, p, q);
+        }
+        if(root->right == nullptr){
+            temp2 = nullptr;
+        }
+
+        if(temp1 != nullptr && temp2 != nullptr){
+            return root;
+        }
+        else if(temp1 == nullptr){
+            return temp2;
+        }
+        else{
+            return temp1;
+        }
     }
 };
