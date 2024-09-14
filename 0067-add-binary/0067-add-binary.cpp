b31629ad1c4a1356119d74ba990f086885d06d04
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-09-14 21:05:37 +0200

    Time: 0 ms (100%), Space: 8.3 MB (49.94%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    void add(){
+
+    }
+    string addBinary(string a, string b) {
+        if(a.length() > b.length()){
+            b.insert(b.begin(), a.length()-b.length(),'0');
+        }
+        else{
+            a.insert(a.begin(), b.length()-a.length(),'0');
+        }
+
+        bool temp = false;
+        int a_len = a.length();
+        char* point1 = &a[a_len-1];
+        char* point2 = &b[b.length()-1];
+        std::string result(a_len+1, '\0');
+
+        for(int i=result.length()-1; i>0; i--){
+            if((int)*point1-'0' ^ (int)*point2-'0'){
+                if(temp){
+                    result[i] = '0';
+                }
+                else{
+                    result[i] = '1';
+                }
+            }
+            else{
+                if(temp){
+                    result[i] = '1';
+                }
+                else{
+                    result[i] = '0';
+                }
+            }
+            temp = ((int)*point1-'0' & (int)*point2-'0') ^ (((int)*point1-'0' ^ (int)*point2-'0') & temp);
+            point1--;
+            point2--;
+        }
+        if(temp){
+            result[0] = '1';
+        }
+        else{
+            result.erase(result.begin());
+        }
+        return result;
+    }
+};
