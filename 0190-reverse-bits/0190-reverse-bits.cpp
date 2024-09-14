ca949ad5b4058eb293ef7069496bde759ea94ced
Piotr Ziobrowski <130161133+ziobrowskipiotr@users.noreply.github.com>
2024-09-15 00:57:24 +0200

    Time: 0 ms (100%), Space: 7.4 MB (69.82%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -3,20 +3,14 @@ public:
     uint32_t reverseBits(uint32_t n) {
         uint32_t mask = 1<<31;
         uint32_t point = 1;
-        uint32_t g = n & mask;
-        cout<<g<<endl;
+        bool bol1;
+        bool bol2;
         for(int i=0; i<16; i++){
-            if(n & mask){
-                if(!(n & point)){
-                    n = n ^ point;
-                    n = n ^ mask;
-                }
-            }
-            else{
-                if(n & point){
-                    n = n ^ point;
-                    n = n ^ mask;
-                }
+            bol1 = n & point;
+            bol2 = n & mask;
+            if(bol1 != bol2){
+                n = n ^ point;
+                n = n ^ mask;
             }
             mask = mask>>1;
             point = point<<1;
